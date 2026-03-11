dcd47ea672c780bba988aed8ea71224c80a32d35
tomatorivera <tomatorivera@gmail.com>
2026-03-11 10:39:41 -0300

    feat: add customer entity, DTO and mapper

---
+++ msvc-customer/src/main/java/com/trivera/msvc_customer/dto/CustomerDTO.java
@@ -0,0 +1,25 @@
+package com.trivera.msvc_customer.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDTO {
+
+  private UUID id;
+  private String cbu;
+  private String name;
+  private String email;
+  private String mobile;
+  private String address;
+
+}
