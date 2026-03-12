1ce1fcc86601542f6d0a3ee00d9d3087519d882f
tomatorivera <tomatorivera@gmail.com>
2026-03-12 17:44:27 -0300

    feat: add feign client for customer communication
    Used to save the customer data during the account creation process.

--- msvc-account/src/main/java/com/trivera/msvc_account/MsvcAccountApplication.java
+++ msvc-account/src/main/java/com/trivera/msvc_account/MsvcAccountApplication.java
@@ -2,7 +2,9 @@ package com.trivera.msvc_account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsvcAccountApplication {
 
