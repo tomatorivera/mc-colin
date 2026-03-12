99e447f08396c56bc86af63f4a676ecf75523d4b
tomatorivera <tomatorivera@gmail.com>
2026-03-12 13:11:45 -0300

    chore: initial project setup and dependencies for account service

---
+++ msvc-account/src/main/java/com/trivera/msvc_account/MsvcAccountApplication.java
@@ -0,0 +1,13 @@
+package com.trivera.msvc_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcAccountApplication.class, args);
+	}
+
+}
