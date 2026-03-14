3a930e04c07160d8f93a4262143181b692bb82aa
tomatorivera <tomatorivera@gmail.com>
2026-03-14 13:55:26 -0300

    chore(msvc-credit-disbursement): initial project setup and dependencies

---
+++ msvc-credit-disbursement/src/main/java/com/trivera/msvc_credit_disbursement/MsvcCreditDisbursementApplication.java
@@ -0,0 +1,13 @@
+package com.trivera.msvc_credit_disbursement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcCreditDisbursementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcCreditDisbursementApplication.class, args);
+	}
+
+}
---
+++ msvc-credit-disbursement/src/test/java/com/trivera/msvc_credit_disbursement/MsvcCreditDisbursementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trivera.msvc_credit_disbursement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCreditDisbursementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
